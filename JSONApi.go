97463f306d0de660d1435444765f42d5955fcce4
Dario <dario.knezovich@gmail.com>
2018-03-19 13:36:12 +0100

    adding JSONApi.go

---
+++ JSONApi.go
@@ -0,0 +1,21 @@
+package GoLang_API_2
+
+import "net/http"
+
+func optionsRequest(w http.ResponseWriter, r *http.Request) {
+	(w).Header().Set("Access-Control-Allow-Origin", "*")
+	(w).Header().Set("Access-Control-Allow-Origin", "*")
+	(w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+	(w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+	w.WriteHeader(http.StatusOK)
+}
+
+func setupResponse(w *http.ResponseWriter, r *http.Request) {
+	(*w).Header().Set("Access-Control-Allow-Origin", "*")
+	(*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+	(*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+}
+
+func getMessage(w http.ResponseWriter, r *http.Request) {
+
+}
--- main.go
+++ main.go
@@ -1,8 +1,21 @@
 package GoLang_API_2
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+)
 
+func handleRequest() {
 
+	myRouter := mux.NewRouter().StrictSlash(true)
+
+	myRouter.HandleFunc("/match", getMessage).Methods("POST")
+	myRouter.HandleFunc("/match", optionsRequest).Methods("OPTIONS")
+
+	log.Fatal(http.ListenAndServe(":8082", myRouter))
+}
 func main() {
 	fmt.Println("GoLang API for parsing messages")
 	handleRequest()
